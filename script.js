6feabc7e4257af5c1d62773b75d434d306715927
Timo Indlekofer <timo.indlekofer@web.de>
2026-03-25 14:26:07 +0100

    index.html updated:
    - Dialog box updated
    
    style.css updated:
    
    - css classes for dialog box updated
    
    script.js updated:
    
    - remove file ending added
    
    pictures for buttons added

--- script.js
+++ script.js
@@ -5,25 +5,27 @@ const dialogBox = document.getElementById('dialog-box');
 const pictureFullView = document.getElementById('picture-full-view');
 const closeButton = document.getElementById('close-button-dialog-box');
 const galleryPictures = document.querySelectorAll('.gallery-item');
+const dialogTitle = document.getElementById('dialog-title');
 
 
 // Click on picture in gallery: Open dialog box
 galleryPictures.forEach(element => {
     element.addEventListener('click', () => {
+
+        // Open dialog box
         dialogBox.showModal();
         pictureFullView.src = element.src;
+
+        // Get full file name from URL
+        let fileName = element.src.split('/').pop();
+
+        // Remove file ending
+        let removeFileEnding = fileName.slice(0, fileName.lastIndexOf('.'));
+
+        // Add file name (without file ending OR full file name)
+        dialogTitle.textContent = "Picture name: " + removeFileEnding || fileName;
     });
 });
-
-
-
-// Select all images in the gallery
-// document.querySelectorAll('.gallery-item').forEach (element => {
-//     element.addEventListener("click", () => {
-//         dialogBox.showModal();
-//         pictureFullView.src = element.src;
-//     });
-// });
 
 
 // Click on close button: Close dialog
